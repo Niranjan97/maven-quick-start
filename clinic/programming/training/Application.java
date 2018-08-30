37dcb03046f8b0ad1ee0e6b190592947c4931148
niranjan kumar <niranjankumar.v@sensiple.com>
2018-08-30 15:33:23 +0530

    using string utils and maven central

--- clinic/programming/training/Application.java
+++ clinic/programming/training/Application.java
@@ -3,7 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 public class Application {
     
-    
+		public int countwords(String words)
+		{
+			String[] seperateWords= StringUtils.split(words,' ');
+			return(seperateWords==null)?0:seperateWords.length;
+		}
+		
 		public void greet()
 		{
 			List<String> greetings =new ArrayList<>();
@@ -22,5 +27,7 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application obj = new Application();
 		obj.greet();
+		int count =app.countwords("I have four words");
+		System.out.println("Word Count: "+ count);
     }
 }
